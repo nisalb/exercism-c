a14233f17d6ad2a126922ed20a89e7f84078a8e2
Nisal D Bandara <nisaldbandara96@gmail.com>
2020-03-13 09:57:21 +0530

    isogram: no reason

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -4,38 +4,38 @@
 
 #define TABSIZE 26
 
-unsigned bitmap_toggle(unsigned, unsigned);
-bool bitmap_isset(unsigned, unsigned);
+unsigned bitmap_toggle (unsigned, unsigned);
+bool     bitmap_isset (unsigned, unsigned);
 
 unsigned
-bitmap_toggle(unsigned bitmap, unsigned pos)
+bitmap_toggle (unsigned bitmap, unsigned pos)
 {
     return bitmap ^ ((unsigned) 1 << pos);
 }
 
 bool
-bitmap_isset(unsigned bitmap, unsigned pos)
+bitmap_isset (unsigned bitmap, unsigned pos)
 {
     return (bitmap & ((unsigned) 1 << pos)) > 0;
 }
 
 bool
-is_isogram(const char phrase[])
+is_isogram (const char phrase[])
 {
     if (!phrase)
         return false;
 
-//    bool chartab[TABSIZE];
+    //    bool chartab[TABSIZE];
     unsigned bitmap = 0;
 
     const char* pp = phrase;
     while (*pp) {
-        unsigned idx = tolower(*pp) - 'a';
+        unsigned idx = tolower (*pp) - 'a';
         if (idx <= TABSIZE) {
-            if (bitmap_isset(bitmap, idx))
+            if (bitmap_isset (bitmap, idx))
                 return false;
             else
-                bitmap = bitmap_toggle(bitmap, idx);
+                bitmap = bitmap_toggle (bitmap, idx);
         }
         pp++;
     }
